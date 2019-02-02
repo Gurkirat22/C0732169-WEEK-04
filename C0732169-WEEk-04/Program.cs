09625b768c503d4901854e95dfc13988b0c3cfdb
visualstudio <visualstudio@122-PC07>
2019-02-01 21:34:42 -0500

    9:20

--- C0732169-WEEk-04/Program.cs
+++ C0732169-WEEk-04/Program.cs
@@ -10,6 +10,52 @@ namespace C0732169_WEEk_04
     {
         static void Main(string[] args)
         {
+            Countryside c = new Countryside();
+            c.run();
+            c.Travel();
+            Console.ReadLine();
+        }
+    }
+
+    class Village
+    {
+        public bool isAstrilde = false;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+
+    class Countryside
+    {
+        Village Toronto, Ajax, Maple;
+
+        public Village CurrentVillage { get; private set; }
+
+        public void run()
+        {
+            Maple = new Village();
+            Maple.villageName = "Maple";
+            Toronto = new Village();
+            Toronto.villageName = "Toronto";
+            Ajax = new Village();
+            Ajax.isAstrilde = true;
+            Ajax.villageName = "Ajax";
+
+            Maple.nextVillage = Toronto;
+            Toronto.nextVillage = Ajax;
+            Ajax.nextVillage = null;
+        }
+
+        public void Travel()
+        {
+            CurrentVillage = Toronto;
+            while (true)
+            {
+                if (CurrentVillage.isAstrilde)
+                    Console.WriteLine("Astrilde is in : " + CurrentVillage.villageName);
+                else
+                    CurrentVillage = CurrentVillage.nextVillage;
+            }
         }
     }
 }
